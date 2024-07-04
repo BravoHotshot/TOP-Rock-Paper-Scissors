8cc2f8383d4bc82aba188aff58e21cc084e39ebf
Bravo Hotshot <98163699+BravoHotshot@users.noreply.github.com>
2024-07-04 11:27:33 +0530

    Add variables to keep track of scores

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 // 1: Paper
 // 2: Scissors
 
+let humanScore = 0;
+let computerScore = 0;
+
 let getComputerChoice = () => Math.floor(Math.random() * 3);
 
 let getHumanChoice = () => prompt("Enter 'rock', 'paper' or 'scissors'");
